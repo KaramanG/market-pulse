3c238702c03ec42636a63738ce9e7d93a4b03c49
Dmitry <dima.kornakow@yandeex.ru>
2025-07-16 20:36:27 +0300

    изменил расположение блоков как в презентации

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -51,23 +51,7 @@ function App() {
         </aside>
         
         <main className="main-section main-grid">
-          <div className="page-content">
-            <section className="filters" aria-label="Фильтры данных">
-              <select className="filter-select">
-                <option value="day">Период: день</option>
-                <option value="week">Период: неделя</option>
-                <option value="month">Период: месяц</option>
-              </select>
-              <select className="filter-select">
-                <option value="scheduled">Запланированные счета</option>
-                <option value="paid">Оплаченные счета</option>
-              </select>
-              <select className="filter-select">
-                <option value="gaps">Даты разрывов</option>
-                <option value="payments">Даты платежей</option>
-              </select>
-            </section>
-            
+          <div className="page-content">            
             <section className="view-switcher-section" aria-label="Переключатель вида">
               <h2 className="view-title">Обзор</h2>
               <nav className="tabs-nav">
@@ -82,6 +66,22 @@ function App() {
                   </button>
                 ))}
               </nav>
+            </section>
+
+            <section className="filters" aria-label="Фильтры данных">
+              <select className="filter-select">
+                <option value="day">Период: день</option>
+                <option value="week">Период: неделя</option>
+                <option value="month">Период: месяц</option>
+              </select>
+              <select className="filter-select">
+                <option value="scheduled">Запланированные счета</option>
+                <option value="paid">Оплаченные счета</option>
+              </select>
+              <select className="filter-select">
+                <option value="gaps">Даты разрывов</option>
+                <option value="payments">Даты платежей</option>
+              </select>
             </section>
             
             <section className="chart-section" aria-label="Финансовая аналитика">
